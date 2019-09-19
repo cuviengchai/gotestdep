2a51f8e642183f8eaa15127cbc577e8e41bcbc4f
Chawit Uviengchai <chawitu@set.or.th>
2019-09-19 09:39:07 +0700

    fix 2

---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import v "gotestdep/version"
+
+func main() {
+	v.Print()
+}
---
+++ version/version.go
@@ -0,0 +1,8 @@
+package version
+
+import "fmt"
+
+// Print check version
+func Print() {
+	fmt.Println("This is version 1")
+}
